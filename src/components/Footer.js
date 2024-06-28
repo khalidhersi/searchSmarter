8abc84adb0b5780c4f4441342f8e034baa52bba3
Khalid Hersi <cipherpolfoundation@gmail.com>
2024-06-29 00:16:18 +0100

    reinstal npm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <p>&copy; 2024 Search Smarter. All rights reserved.</p>
+        <p>&copy; 2024 SearchSmarter. All rights reserved.</p>
       </div>
     </footer>
   );
